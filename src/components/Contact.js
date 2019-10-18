ce73987f13e7d2a2aa61ce06e62784c95ae7b9d5
Yr0z <y.cocheteau@gmail.com>
2019-10-18 09:54:45 +0200

    new mod online/offline by click

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,35 @@
 import React from 'react'
 import './Contact.css'
 
-const Contact = ({avatar, name, online}) => {
-    return (
-       <div className="Contact">
-       <img className="avatar" src={avatar} alt={name}/>
-       <div className='contain'>
-      <p className='name'>{name}</p>
-      
-      <div className='status'>
-      <div className={online? 'status-online': 'status-offline'}></div>
-      <p className='status-text'>
-             {online? 'online': 'offline'}
-      </p>
-     
-     </div>
-     </div>
- </div>
- 
-
-    );
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.online,
+    };
   }
+  render () {
+    return (
+      <div className="Contact">
+        <img className="avatar" src={this.props.avatar} alt={this.props.name}/>
+        <div className='contain'>
+          <p className='name'>{this.props.name}</p>
+      
+      <div className='status'
+      onClick={envent => {
+          const newOnline = !this.state.online;
+          this.setState({ online: newOline }) }}>
+      <div className="status-text">
+      <div className={this.state.online ? "status-online" : "status-offline"}>
+      </div>
+        {this.state.online ? "online" : "offline"}
+      </div>
+      </div>
+   </div>
+   </div>
+     );
+   }
+}
+
 
 export default Contact
